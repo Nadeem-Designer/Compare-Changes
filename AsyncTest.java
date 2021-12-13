b05c5c3061a0c53b4652b7aaeebc09f842078d94
Nadeem-Designer <nadeem2488@gmail.com>
2021-12-13 10:48:28 +0530

    Create AsyncTest.java

---
+++ AsyncTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+public class AsyncTest {
+    @Test
+    public void testChatRoom() throws Exception {
+        try (final WebClient webClient = new WebClient()) {
+            final HtmlPage page = webClient.getPage(ParameterUtils.getBaseUrl() + "/async/index.html");
+            final HtmlForm form = page.getForms().get(0);
+            final HtmlTextInput textField = form.getInputByName("msg");
+            textField.type("hello");
+            final HtmlSubmitInput button = form.getInputByValue("Send");
+            final HtmlPage page2 = button.click();
+            Thread.sleep(4000);
+        final DomElement msgs = page2.getElementById("msgs");
+            Assert.assertEquals("hello", msgs.asText());
+        }
+    }
+}
